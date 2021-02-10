2cffd79822a0b25957faa756b72b307df6c83b80
HendrikWeisgerber <57496555+HendrikWeisgerber@users.noreply.github.com>
2021-02-10 13:53:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Allgemeines
+import React, { Component } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+//Pages
+import Test from "./Pages/Test2"
+
+
+class App extends Component {
+ 
+  render(){
+    //Links
+    const HomeLink="/";
+
+
+
+    return<Router>
+        <Switch>
+            <Route exact path={HomeLink} component = {Test} />
+
+        </Switch>
+    </Router>
+
+  }
 }
-
 export default App;
