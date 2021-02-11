129d1132724c6ac1a75d4da224f2bb6d165dd70e
HendrikWeisgerber <57496555+HendrikWeisgerber@users.noreply.github.com>
2021-02-11 15:09:12 +0100

    wieder lauffähig und mit Fehlerumleitung

--- src/App.js
+++ src/App.js
@@ -11,12 +11,9 @@ import {
 
 //Pages
 import Test from "./Pages/Test2"
-<<<<<<< HEAD
 import Fehler from "./Components/Fehler/FehlerPage"
-=======
 import DWE from "./Pages/DWE"
 
->>>>>>> 0868420763d96a906b0ce48f2df237d64fdff111
 
 
 class App extends Component {
@@ -24,27 +21,21 @@ class App extends Component {
   render(){
     //Links
     const HomeLink="/";
-<<<<<<< HEAD
     const FehlerLink="/Fehler404";
-
-
-=======
     const DWELink = "/DWE";
->>>>>>> 0868420763d96a906b0ce48f2df237d64fdff111
+
+    
 
     return<Router>
         <Switch>
             <Route exact path={HomeLink} component = {Test} />
-<<<<<<< HEAD
             <Route exact path={FehlerLink} component = {Fehler} />
+            <Route exact path={DWELink} component = {DWE} />
 
 
+           
 
             <Redirect to ={FehlerLink}/>
-
-=======
-            <Route exact path={DWELink} component = {DWE} />
->>>>>>> 0868420763d96a906b0ce48f2df237d64fdff111
         </Switch>
     </Router>
 
--- src/Components/Fehler/FehlerPage.jsx
+++ src/Components/Fehler/FehlerPage.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ".././App.css";
+import "../.././App.css";
 import Fehler from "./Fehler";
 
 //import { Link } from "react-router-dom";
